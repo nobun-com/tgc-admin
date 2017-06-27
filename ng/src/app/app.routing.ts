44dc3ed0946cc19d1e5983712c1c277cd1d0da27
jagbirs01 <mindnervestech@gmail.com>
2017-06-27 12:42:32 +0530

    second changes

--- ng/src/app/app.routing.ts
+++ ng/src/app/app.routing.ts
@@ -27,10 +27,12 @@ export const AppRoutes: Routes = [{
   }, {
     path: 'tables',
     loadChildren: './tables/tables.module#TablesModule'
-  }, {
-    path: 'charts',
-    loadChildren: './chartlib/chartlib.module#ChartlibModule'
-  }, {
+  }
+  // , {
+  //   path: 'charts',
+  //   loadChildren: './chartlib/chartlib.module#ChartlibModule'
+  // }
+  , {
     path: 'maps',
     loadChildren: './maps/maps.module#MapModule'
   }, {
