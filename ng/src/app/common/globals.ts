1d26ae7145fa591f4e45fcbbca533a454fd5408a
root <mindnervestech@gmail.com>
2017-08-01 12:58:20 +0530

    global.ts

--- ng/src/app/common/globals.ts
+++ ng/src/app/common/globals.ts
@@ -3,7 +3,6 @@ import {  Injectable } from '@angular/core';
 Injectable()
 export class Globals {
 
-      SERVERADDRESS = 'http://ec2-13-228-203-173.ap-southeast-1.compute.amazonaws.com:8080/';
-
-      //SERVERADDRESS = 'http://192.168.1.55:8080/';
+      SERVERADDRESS = 'http://ec2-13-228-203-173.ap-southeast-1.compute.amazonaws.com:8080/'; // ec2 server
+      //SERVERADDRESS = 'http://localhost:8080/'; // local server
 }
